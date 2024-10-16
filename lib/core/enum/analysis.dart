1fb617324259da7d4496cf042f63476f228f24bc
koki622 <koki08018907926@gmail.com>
2024-10-16 18:03:14 +0900

    解析処理のenumを修正。

--- lib/core/enum/analysis.dart
+++ lib/core/enum/analysis.dart
@@ -1,42 +1,32 @@
 // 解析処理の進捗
 enum AnalysisType implements Comparable<AnalysisType> {
   // アップロード
-  uploading('uploading'),
+  uploading,
 
   // 分離処理
-  separating('separating'),
+  separating,
 
   // 分離処理結果のzip圧縮処理
-  compression('compression'),
+  compression,
 
   // コード分析
-  chordAnalysis('chordAnalysis'),
+  chordAnalysis,
 
   // スペクトログラム抽出
-  extractingSpectrograms('extractingSpectrograms'),
+  extractingSpectrograms,
 
   // 音楽構造分析
-  structureAnalysis('structureAnalysis'),
+  structureAnalysis,
 
   // 歌詞分析
-  lyricAnalysis('lyricAnalysis'),
+  lyricAnalysis,
 
   // 結果のダウンロード
-  downloadSeparationResult('downloadSeparationResult'),
-  downloadChordAnalysisResult('downloadChordAnalysisResult'),
-  downloadStructureAnalysisResult('downloadStructureAnalysisResult'),
-  downloadLyricAnalysisResult('downloadLyricAnalysisResult');
-
-  const AnalysisType(this.value);
-  final String value;
+  downloadSeparationResult,
+  downloadChordAnalysisResult,
+  downloadStructureAnalysisResult,
+  downloadLyricAnalysisResult;
 
   @override
   int compareTo(AnalysisType other) => name.compareTo(other.name);
-
-  static AnalysisType fromString(String value) {
-    return AnalysisType.values.firstWhere(
-      (element) => element.value == value,
-      orElse: () => throw ArgumentError('Invalid AnalysisType: $value'),
-    );
-  }
 }
